5971407a689bb619e16276e71b47074b75988c1c
김민창 <rud0806@gmail.com>
2021-03-22 19:46:08 +0900

    adding lable var code

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     //Outlet은 UI Interface에 올라와있는 어떠한 값을 가져오고 싶거나 코드에서 활용하고 싶을때 활용
+    
+    var randomValue: Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!    //스토리보드 위에 올려져있는 요소와 연결할 때 쓰는 어노테이션
+    @IBOutlet weak var tryCountLable: UILabel!
+    @IBOutlet weak var sliderValueLable: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal) //stroyboard로 이미지를 바꿀 수 없을때 코드로 바꾸는 것이 가능하다.
+        reset()
     }
 
     //인터페이스 빌더에 요소들이 이벤트를 받았을때 거기에 반응하기 위한 것.
@@ -28,8 +34,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton){
-        slider.value = 15
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        slider.value = 15
+        print("reset")
     }
 }
 
