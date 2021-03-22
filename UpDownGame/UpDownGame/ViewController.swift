312570aa4e44878cd29b1adfe99b1f64bbb0171a
김민창 <rud0806@gmail.com>
2021-03-22 20:21:18 +0900

    adding alert

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -29,10 +29,42 @@ class ViewController: UIViewController {
     //인터페이스 빌더에 요소들이 이벤트를 받았을때 거기에 반응하기 위한 것.
     @IBAction func sliderValueChanged(_ sender: UISlider){  //이벤트에 반응하는 액션 어노테이션
         print(sender.value)
+        sliderValueLabel.text = "\(Int(slider.value))"
+    }
+    
+    func showAlert(message: String) {
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in self.reset() }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func touchUpHitButton(_ sender: UIButton){
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount += 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            print("YOU HIT!!")
+            showAlert(message: "YOU HIT!!")
+            reset()
+        } else if tryCount >= 5 {
+            print("YOU LOSE..")
+            showAlert(message: "YOU LOSE..")
+            reset()
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton){
@@ -44,6 +76,14 @@ class ViewController: UIViewController {
         print("reset")
         randomValue = Int.random(in: 0...30) //... -> 범위 연산자의 일종 A...B 폐쇄 범위 연산자 A..<B 반폐쇄연산자는 앞의 수 포함 뒤의 수를 포함하지 않는다.
                                              //A..., ...A, ..<A 단방향 범위 연산자 또한 3가지 있다.
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
         
     }
 }
