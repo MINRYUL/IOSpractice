20c860bc092a1748018966a5d9d9ccdba48d40dd
김민창 <rud0806@gmail.com>
2021-03-30 15:37:35 +0900

    push pop interface implementation

---
+++ SignUp/SignUp/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  SignUp
+//
+//  Created by 김민창 on 2021/03/30.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func popToPrev() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
