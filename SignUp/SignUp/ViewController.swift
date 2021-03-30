69dc255726e7615dd99662a0a9ea6d27077afbea
김민창 <rud0806@gmail.com>
2021-03-30 17:41:02 +0900

    state change method

--- SignUp/SignUp/ViewController.swift
+++ SignUp/SignUp/ViewController.swift
@@ -12,8 +12,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("ViewController의 view가 메모리에 로드")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewController의 view가 화면에 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("viewController의 view가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("viewController의 view가 화면에서 사라질 예정")
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("viewController의 view가 subview를 레이아웃 하려함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("viewController의 view가 subview를 레이아웃 함")
+    }
 
 }
 
