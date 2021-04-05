6ee01cfa333ee950ca07816da936bc037102f8b7
김민창 <rud0806@gmail.com>
2021-04-05 20:06:13 +0900

    using tableview

---
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 김민창 on 2021/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return korean.count
+        case 1:
+            return english.count
+        default:
+            return 0
+        }
+    }//섹션에 해당하는 row의 개수가 몇개가 되어야 하는지
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndentifier, for: indexPath)
+        
+        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+        cell.textLabel?.text = text
+        
+        return cell
+    }//해당 로우에 맞는 셀을 돌려달라는 것
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 { return "한글" }
+        else { return "영어" }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellIndentifier: String = "cell"
+    
+    let korean: [String] = ["가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"]
+    let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+        //코드로 연결
+    }
+
+}
+
