d27ac99cd4e0ab81e34646d528624b7596a7e539
김민창 <rud0806@gmail.com>
2021-04-05 20:16:34 +0900

    adding section button

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             return korean.count
         case 1:
             return english.count
+        case 2:
+            return dates.count
         default:
             return 0
         }
@@ -24,19 +26,26 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndentifier, for: indexPath)
         
-        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
-        cell.textLabel?.text = text
+        if indexPath.section < 2 {
+            let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+            cell.textLabel?.text = text
+        }else{
+            cell.textLabel?.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+        }
         
         return cell
     }//해당 로우에 맞는 셀을 돌려달라는 것
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 { return "한글" }
-        else { return "영어" }
+        if section < 2 {
+            if section == 0 { return "한글" }
+            else { return "영어" }
+        }
+        return nil
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
     
     @IBOutlet weak var tableView: UITableView!
@@ -44,7 +53,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let korean: [String] = ["가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"]
     let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var dates: [Date] = []
+    
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }()
 
+    @IBAction func touchUpAddButton(_ sender: UIButton) {
+        dates.append(Date())
+        
+//        self.tableView.reloadData()
+        //전체의 데이터를 다 가져오기 떄문에 비효율적
+        self.tableView.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
