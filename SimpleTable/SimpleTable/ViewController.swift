8217effd77a25fbcd6b8b44e4326b1036dedd25b
김민창 <rud0806@gmail.com>
2021-04-06 14:12:40 +0900

    using custom cell

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -7,7 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellIndentifier: String = "cell"
+    let customCellIdentifier: String = "customCell"
+    
+    let korean: [String] = ["가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"]
+    let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var dates: [Date] = []
+    
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
+    let timeFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.timeStyle = .medium
+        return formatter
+    }()
+
+    @IBAction func touchUpAddButton(_ sender: UIButton) {
+        dates.append(Date())
+        
+        //self.tableView.reloadData()
+        //전체의 데이터를 다 가져오기 떄문에 비효율적
+        self.tableView.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+        //코드로 연결
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
@@ -23,17 +58,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }//섹션에 해당하는 row의 개수가 몇개가 되어야 하는지
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndentifier, for: indexPath)
-        
         if indexPath.section < 2 {
+            let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndentifier, for: indexPath)
             let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
             cell.textLabel?.text = text
+            return cell
         }else{
-            cell.textLabel?.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+            let cell: CustomTableViewCell = tableView.dequeueReusableCell(withIdentifier: self.customCellIdentifier, for: indexPath) as! CustomTableViewCell
+            //위를 보면 강제 캐스팅을 하고 있는데 더 좋은 방법이 있으니 찾아볼것.
+            cell.leftLabel.text = self.dateFormatter.string(from: self.dates[indexPath.row])
+            cell.rightLabel.text = self.timeFormatter.string(from: self.dates[indexPath.row])
+            
+            return cell
         }
         
-        return cell
     }//해당 로우에 맞는 셀을 돌려달라는 것
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -46,35 +84,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 3
-    }
-    
-    @IBOutlet weak var tableView: UITableView!
-    let cellIndentifier: String = "cell"
-    
-    let korean: [String] = ["가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"]
-    let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    var dates: [Date] = []
-    
-    let dateFormatter: DateFormatter = {
-        let formatter: DateFormatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .medium
-        return formatter
-    }()
-
-    @IBAction func touchUpAddButton(_ sender: UIButton) {
-        dates.append(Date())
-        
-//        self.tableView.reloadData()
-        //전체의 데이터를 다 가져오기 떄문에 비효율적
-        self.tableView.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        self.tableView.delegate = self
-//        self.tableView.dataSource = self
-        //코드로 연결
     }
 
 }
