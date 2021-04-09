882aabfca04992265974fd7cf1110f89e1072f50
김민창 <rud0806@gmail.com>
2021-04-09 21:46:19 +0900

    delete photo

--- PhotosExample/PhotosExample/ViewController.swift
+++ PhotosExample/PhotosExample/ViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, PHPhotoLibraryChangeObserver {
+   
+    func photoLibraryDidChange(_ changeInstance: PHChange) { //변화를 하면 반응을 하겠다.
+        guard let changes = changeInstance.changeDetails(for: fetchResult)
+        else { return }
+        
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadData()
+        }
+    }
+    
     
     @IBOutlet weak var tableView: UITableView!
     var fetchResult: PHFetchResult<PHAsset>!
     let imageManager: PHCachingImageManager = PHCachingImageManager() //가져온 에셋으로 이미지를 로드
     let cellIdentifier: String = "cell"
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete { //에셋이 삭제냐?
+            let asset: PHAsset = self.fetchResult[indexPath.row]
+            
+            PHPhotoLibrary.shared().performChanges({ PHAssetChangeRequest.deleteAssets([asset] as NSArray)}, completionHandler: nil) //딜레드 에셋을 한다.
+            // 실제로 사진첩의 사진이 삭제된다. 얼랏 창이 뜨면서 삭제가 됨.
+        }
+    }
     func requestCollection() {
         let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil) //카메라롤 컬랙션을 가져옴
         
@@ -64,6 +88,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         default:
             print("몰랑")
         }
+        PHPhotoLibrary.shared().register(self)
         // Do any additional setup after loading the view.
         
     }
