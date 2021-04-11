3c64ba6f45dd47aba2722fdde905cc3789b657d7
김민창 <rud0806@gmail.com>
2021-04-11 17:48:15 +0900

    collection view example

---
+++ FriendsCollection/FriendsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by 김민창 on 2021/04/11.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+    
+}
