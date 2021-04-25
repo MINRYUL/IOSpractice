28adf768f578e5845f917ea5f6e96f24d7e926e7
김민창 <rud0806@gmail.com>
2021-04-25 22:06:18 +0900

    adding Tap Gesture

--- SnapKitLoginScreen/SnapKitLoginScreen/Controller/ViewController.swift
+++ SnapKitLoginScreen/SnapKitLoginScreen/Controller/ViewController.swift
@@ -28,12 +28,28 @@ class ViewController: UIViewController {
         $0.backgroundColor = .systemTeal
         $0.layer.cornerRadius = 10
     }
+    private var signUpLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14)
+        $0.textColor = .systemBlue
+        $0.text = "회원이 아니신가요?"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        self.setupLabelTap()
         self.setupMainLayoutWithSnapKit()
+    }
+    
+    @objc func labelTapped(_ sender: UITapGestureRecognizer) {
+        print("labelTapped")
+    }
+    
+    private func setupLabelTap() {
+        let SignUpLabelTap = UITapGestureRecognizer(target: self, action: #selector(self.labelTapped(_:)))
+        self.signUpLabel.isUserInteractionEnabled = true
+        self.signUpLabel.addGestureRecognizer(SignUpLabelTap)
     }
     
     private func setupMainLayoutWithSnapKit() {
@@ -41,6 +57,7 @@ class ViewController: UIViewController {
         view.addSubview(idField)
         view.addSubview(pwField)
         view.addSubview(loginButton)
+        view.addSubview(signUpLabel)
         
         titleimage.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(100)
@@ -67,6 +84,13 @@ class ViewController: UIViewController {
             make.top.equalTo(pwField.snp.bottom).offset(16)
             make.leading.equalTo(pwField.snp.leading).offset(-5)
             make.trailing.equalTo(pwField.snp.trailing).offset(5)
+            make.height.equalTo(44)
+        }
+        
+        signUpLabel.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(3)
+            make.leading.equalTo(loginButton.snp.leading).offset(230)
+            make.trailing.equalTo(loginButton.snp.trailing).offset(0)
             make.height.equalTo(44)
         }
     }
